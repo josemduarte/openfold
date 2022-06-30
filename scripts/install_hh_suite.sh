76e4aa8e616297b5289b5200cbf30e1057087d8a
Jose Duarte <jose.m.duarte@gmail.com>
2022-06-30 13:39:17 -0700

    Parameterized install_hh_suite

--- scripts/install_hh_suite.sh
+++ scripts/install_hh_suite.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-git clone --branch v3.3.0 https://github.com/soedinglab/hh-suite.git /tmp/hh-suite \
-  && mkdir /tmp/hh-suite/build \
-  && pushd /tmp/hh-suite/build \
-  && cmake -DCMAKE_INSTALL_PREFIX=/opt/hhsuite .. \
+set -e
+
+if [ -z "$1" ]
+then
+  echo "Usage: $0 <target install dir> [-l]"
+  echo "Install hh-suite to provided target install dir. If second argument is -l, then a symlink to /usr/bin is created"
+  exit 1
+fi
+
+install_dir=$1
+TMP_DIR=/tmp/hh-suite
+
+git clone --branch v3.3.0 https://github.com/soedinglab/hh-suite.git $TMP_DIR \
+  && mkdir $TMP_DIR/build \
+  && cd $TMP_DIR/build \
+  && cmake -DCMAKE_INSTALL_PREFIX="$install_dir" .. \
   && make -j 4 && make install \
-  && ln -s /opt/hhsuite/bin/* /usr/bin \
-  && popd \
-  && rm -rf /tmp/hh-suite
+
+
+if [ "$2" = "-l" ]
+then
+  ln -s "$install_dir"/bin/* /usr/bin
+fi
+
+rm -rf $TMP_DIR
